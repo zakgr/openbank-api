4e8700e5b10c8d8b715ca38521443c96691670b2
ΚΟΥΒΑΡΗΣ ΘΕΟΦΑΝΗΣ (E40496) <kouvaris.th@nbg.gr>
2016-05-26 10:59:00 +0300

    accounts , autobuild from vscode, register, default view

---
+++ build.bat
@@ -0,0 +1,10 @@
+@ECHO OFF
+::specify folder path of project
+cd BankOpenNodeTsApi/OBPNodeTsApi
+
+::run build commands
+CMD /C npm install --save
+CMD /C npm run tsc
+
+::exit
+Goto :eof
---
+++ build.sh
@@ -0,0 +1,6 @@
+
+cd BankOpenNodeTsApi/OBPNodeTsApi
+
+npm install --save
+npm run tsc
+exit
